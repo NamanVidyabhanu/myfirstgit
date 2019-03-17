e537ee4335c06c6a298aa36171ca27ec3cee5f82
Naman <vidyabhanuna@rknec.edu>
2019-03-17 23:46:13 +0530

    new changes

--- auction/views.py
+++ auction/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Bid_desk
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'auction/post_list.html', {})
+    bid_desks=Bid_desk.objects.order_by('name')
+    return render(request, 'auction/post_list.html', {'bid_desks':bid_desks})
